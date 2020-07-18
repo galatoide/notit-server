4fda98e0ceb846a4359fcdbeb912850f19c3c396
Wilson Cruz <galatoide@gmail.com>
2020-07-18 17:04:19 +0100

    AWS

--- app.js
+++ app.js
@@ -27,6 +27,7 @@ const app = express();
 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
   next();
 });
 
