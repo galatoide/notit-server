592f07fd71bf5fdda3cc235517ddf1ab6cdd3d4e
Wilson Cruz <galatoide@gmail.com>
2020-07-22 21:54:46 +0100

    heroku

--- configs/passport.js
+++ configs/passport.js
@@ -1,6 +1,6 @@
 const JwtStrategy = require("passport-jwt").Strategy;
 const ExtractJwt = require("passport-jwt").ExtractJwt;
-const User = require("../models/User");
+const User = require("../models/User.js");
 const SECRET = process.env.SECRET;
 
 const opts = {};
