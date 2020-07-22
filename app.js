f450add89f8135cd0b0d708a56b0fac48dae7191
Wilson Cruz <galatoide@gmail.com>
2020-07-22 22:16:21 +0100

    heroku

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const cors         = require('cors');
 const passport     = require("passport");
 
 mongoose
-  .connect(process.env.HEROKU_MONGODB_URI, {useNewUrlParser: true})
+  .connect(process.env.MONGODB_URI, {useNewUrlParser: true})
   .then(x => {
     console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
   })
